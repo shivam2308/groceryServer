1a7af82b6ed7a4d08dcdd24ac56eb37f43ea260d
joshijitendra-18 <joshijitendra2718@gmail.com>
2021-07-18 18:10:11 +0530

    backend changes

--- app/BuyPbModule/BuyHelper.php
+++ app/BuyPbModule/BuyHelper.php
@@ -18,22 +18,26 @@ class BuyHelper
         $this->timeUtility = new TimeUtility();
         $this->m_alphaToIntegerConvertor = new IntegerToAlphaConvertor();
     }
-
+    public function orderIdEncoder($Ids){
+        $id = explode("@",$Ids);
+        $res = $this->m_alphaToIntegerConvertor->toNum($id[1]);
+        return $id[0] . "@" . $res;
+    }
     public function createOrderId($customerId, $itemId)
     {
-
-        $custId = $this->m_alphaToIntegerConvertor->toNum($customerId);
-        $itemId = $this->m_alphaToIntegerConvertor->toNum($itemId);
+        $custId = $this->orderIdEncoder($customerId);
+        $itemId = $this->orderIdEncoder($itemId);
         $currentmilli = $this->timeUtility->getMilliseconds();
-        $orderId = $custId . "@" . $itemId . "#" . $currentmilli;
+        $orderId = $custId . "!" . $itemId . "#" . $currentmilli;
+        error_log($orderId);
         return $orderId;
     }
 
     public function createParentOrderId(string $customerId, string $paymentId)
     {
-        $custId = $this->m_alphaToIntegerConvertor->toNum($customerId);
-        $pId = $this->m_alphaToIntegerConvertor->toNum($paymentId);
-        return $custId . "@" . $pId;
+        $custId = $this->orderIdEncoder($customerId);
+        $pId = $this->orderIdEncoder($paymentId);
+        return $custId . "!" . $pId;
     }
 
 }
